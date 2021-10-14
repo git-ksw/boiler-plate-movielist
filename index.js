c7d1f429cc0e641c1d2b9db39ea769fb4099f52b
git-ksw <sunyell123@gmail.com>
2021-10-14 16:44:05 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const config = require('./config/key')
 
+const {User}  =require("./models/User")
 const mongoose= require('mongoose')
 
-mongoose.connect('mongodb+srv://wook:asd123@wookyoutubeclone.xuxjt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(()=>console.log('MongDB connected'))
+// 바디페서로 클라이언트로 오는 정보를 디코딩, 인코딩 해준다.
+// application /x-www ... 이걸 분석해서
+app.use(bodyParser.urlencoded({extended:true}))
+// json타입된걸 분석해서 가저오게
+app.use(bodyParser.json())
+
+
+mongoose.connect(config.mongoURI).then(()=>console.log('MongDB connected'))
 .catch(err=>console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello 안녕하세요')
+  res.send('Hello 안녕하dddd세요')
+})
+
+app.post('/register', (req, res) => {
+    // 회원가입 할때 필요한 정보들을 client에서 가저오면 그것들을 데이터 베이스에넣는다
+  // 인스턴스 생성
+  const user =new User(req.body)
+  // 몽고 db 메쏘드
+  user.save((err,userInfo)=>{
+    if(err) return res.json({succes:false,err})
+    // 200은 성공햇다는 뜻
+    return res.status(200).json({
+      succes:true
+    })
+  })
 })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
